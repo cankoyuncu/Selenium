cbfc284f9f1fc48e151d27c02d830f8add547d64
Can Koyuncu <99793493+cankoyuncu@users.noreply.github.com>
2024-11-18 18:40:33 +0300

    new file upload
    new file upload

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import Service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-service = Service("./chromedriver")
-driver = webdriver.Chrome(service=service)
+# Chrome sürücüsünü otomatik olarak yönet
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
